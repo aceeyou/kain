d9d2ea338e6bc4b45ca7d0610de4825b0ba9fc0e
AceLogronio <acearwingl@gmail.com>
2024-03-19 23:41:28 +0800

    savepoint

--- client/src/components/InputField.tsx
+++ client/src/components/InputField.tsx
@@ -2,26 +2,40 @@ interface PropType {
   name: string;
   label: string;
   inputType: string;
-  value: string | null;
+  value: string;
   onChange: any | null;
+  pattern?: string;
+  error_msg?: string | null;
 }
 
-function InputField({ name, label, inputType, value, onChange }: PropType) {
+function InputField({
+  name,
+  label,
+  inputType,
+  value,
+  onChange,
+  pattern,
+  error_msg = null,
+}: PropType) {
   return (
-    <div className="inputfield__container">
-      <input
-        className="inputfield__input"
-        type={inputType}
-        value={value}
-        name={name}
-        id={name}
-        onChange={onChange}
-        autoComplete="false"
-        required
-      />
-      <label htmlFor={name} className="inputfield__label">
-        {label}
-      </label>
+    <div className="inputfield__outside-container">
+      <div className="inputfield__container">
+        <input
+          className="inputfield__input"
+          type={inputType}
+          value={value}
+          name={name}
+          id={name}
+          onChange={onChange}
+          autoComplete="false"
+          pattern={pattern}
+          required
+        />
+        <label htmlFor={name} className="inputfield__label">
+          {label}
+        </label>
+      </div>
+      <p className="inputfield__error-message">{error_msg}</p>
     </div>
   );
 }
---
+++ server/database.js
@@ -0,0 +1,42 @@
+import mysql from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
+
+const pool = mysql
+  .createPool({
+    host: process.env.HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSW,
+    database: process.env.DB,
+  })
+  .promise();
+
+export async function getUsers() {
+  const [result] = await pool.query("SELECT * FROM users");
+  return result;
+}
+
+// const users = await getUsers();
+// console.log(users);
+
+export async function getUserWithID(id) {
+  const [result] = await pool.query(`SELECT * FROM users WHERE _id = ?`, [id]);
+  return result[0];
+}
+
+// const user = await getUserWithID(1);
+// console.log(user);
+
+// create user
+export async function registerUser(fullname, email, username, password) {
+  const result = await pool.query(
+    `
+        INSERT INTO users (fullname, email, username, password)
+        VALUES (?, ?, ?, ?)
+    `,
+    [fullname, email, username, password]
+  );
+  const new_id = result.insertId;
+  const query = getUserWithID(new_id);
+  return query;
+}
